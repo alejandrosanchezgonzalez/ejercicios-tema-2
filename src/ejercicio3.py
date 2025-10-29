13ec3f09e0f11ae8b0504c21415be2993abc6795
alejandrosanchezgonzalez <alejandrosanchezgonza18@gmail.com>
2025-10-29 12:00:25 +0100

    nuevo ejercicio3

--- src/ejercicio3.py
+++ src/ejercicio3.py
@@ -1,17 +1,22 @@
 from time import *
 from datetime import *
 
-def calcula_edad(fecha):
-    hoy=date.today()
-    edad=hoy.year - fecha.year
-    if (hoy.month, hoy.day)< (fecha.month, fecha.day):
-        edad-=1
-    cumpleaños= date(hoy.year,fecha.month, fecha.year)
-    if cumpleaños<hoy:
-        cumpleaños=date(hoy.year +1,fecha.month, fecha.day)
-        faltan=(cumpleaños-hoy).days
-    return edad,faltan
 
+def calcula_edad(fecha):
+    hoy = date.today()
+
+    edad = hoy.year - fecha.year
+    if (hoy.month, hoy.day) < (fecha.month, fecha.day):
+        edad -= 1
+    
+    proximo_cumple = date(hoy.year, fecha.month, fecha.day)
+
+
+    if proximo_cumple < hoy:
+            proximo_cumple = date(hoy.year + 1, fecha.month, fecha.day)
+
+    dias_faltan = (proximo_cumple - hoy).days
+    return edad, dias_faltan
 if __name__=="__main__":
     fecha_nacimiento=date(2008,10,8)
     edad,dias=calcula_edad(fecha_nacimiento)
